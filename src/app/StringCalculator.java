6d2db739e6358c30aef9dccd087d7e8320f0e7eb
infinity <53152093+infinity-zee@users.noreply.github.com>
2020-11-20 01:31:07 +0530

    Successful build against basic test case

--- src/app/StringCalculator.java
+++ src/app/StringCalculator.java
@@ -2,7 +2,19 @@ package app;
 
 public class StringCalculator {
     public int Add(String numbers) {
+        int sum;
 
-        return 0;
+        char[] arrayOfNumbers = numbers.replaceAll(",", "").toCharArray();
+        sum = calculateSum(arrayOfNumbers);
+
+        return sum;
+    }
+
+    private int calculateSum(char[] numbers) {
+        int sum = 0;
+        for(char ch : numbers) {
+            sum += Character.getNumericValue(ch);
+        }
+        return sum;
     }
 }
